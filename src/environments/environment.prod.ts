76eb664464c03a7518e2a34b6da5e53e54726c0c
Luis Eduardo Sinisterra Montaño <luiseduardosinisterramontano@gmail.com>
2026-05-27 17:41:43 -0500

    chore: add production environment config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: '/api' // To be configured for prod
+  apiUrl: 'https://gateway-service-kudv.onrender.com' // To be configured for prod
 };
